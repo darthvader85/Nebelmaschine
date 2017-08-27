ee95bf2935921f951013e7aed3889f920d85ea42
darthvader85 <klammerstefan85@gmail.com>
2017-08-27 15:56:37 +0200

    Reworked Code

--- Nebelmaschine.ino
+++ Nebelmaschine.ino
@@ -1,6 +1,6 @@
 /***************************************************
   This is a Script to Control a Fog machine with a
-  heater, a pump and a PT100 as Temperaturesensor.
+  heater, a pump and a Type K Thermocouple as Temperaturesensor.
 
   The control Panel has to Switches as Input (Run, Automatic)
   and a Variable Resistor to controll intensity.
@@ -8,22 +8,15 @@
   There are two LED´s to inditcate Status. Heat and ready.
  ****************************************************/
 
-#include <Adafruit_MAX31865.h>
-#define Loopdelay 1000
+#include <Adafruit_MAX31855.h>
+#define Loopdelay 100
+#define Blinkdelay 1000
 #define AutomaticTimeMax 60000UL
 
-/*
-State 0: heat_up
-State 1:
-State 2:
-State 3:
-State 4:
-State 5:
-State 6:
-state 7:
-*/
+#define MAXDO   2
+#define MAXCS   3
+#define MAXCLK  4
 
-const int S_HEATUP = 0;
 const int S_WAITTILLHOT = 1;
 const int S_CHECKINPUTS = 2;
 const int S_RUN = 3;
@@ -41,27 +34,33 @@ const int HeaterPin = 8;
 const int PumpPin = 9;
 
 const int IntensityPin = 0;
+const float Temp_min=25;
+const float Temp_max=30;
+int blink_Counter=0;
 
 unsigned long waitUntil=0;
 unsigned long waitAutomatic=0;
+unsigned long waitBlink=0;
 
 const int debug=1;
-float FogIntensity ;
+long FogIntensity ;
 unsigned long time;
 
-
-// Use software SPI: CS, DI, DO, CLK
-Adafruit_MAX31865 max = Adafruit_MAX31865(5, 4, 3, 2);
-
-// The value of the Rref resistor. Use 430.0!
-#define RREF 430.0
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Controller for SLE Cyberfog Fog Machine");
+  Serial.print("Internal Temp = ");
+  Serial.println(thermocouple.readInternal());
 
-  //Initialize MAX31865 for Temperature reading
-  max.begin(MAX31865_4WIRE);  // set to 2WIRE or 4WIRE as necessary
+  double c = thermocouple.readCelsius();
+  if (isnan(c)) {
+    Serial.println("Something wrong with thermocouple!");
+  } else {
+    Serial.print("C = "); 
+    Serial.println(c);
+  }
 
   //Initialize all the other Pins
   pinMode(RunSwitchPin, INPUT_PULLUP);
@@ -74,12 +73,12 @@ void setup() {
   digitalWrite(PumpPin, HIGH);
   digitalWrite(ReadyLedPin, HIGH);
   waitUntil += Loopdelay;
-  waitAutomatic=0;
+  waitBlink += Blinkdelay;
 }
 
 
 void loop() {
-  static int state = S_HEATUP;
+  static int state = S_WAITTILLHOT;
   time = millis();
   if ((long)(time - waitUntil) >= 0) {
   if(debug)
@@ -87,27 +86,26 @@ void loop() {
     Serial.print("Time:"); Serial.println(time);
   }
   
-  uint16_t rtd = max.readRTD();
-  float temperature = max.temperature(100, RREF);
-  if(temperature<=20.0)
+  double temperature = thermocouple.readCelsius();
+  if(temperature<=Temp_min)
   {
     digitalWrite(ReadyLedPin, HIGH);
     state = S_WAITTILLHOT;
   }
   switch (state)  {
-    case S_HEATUP:
-      digitalWrite(HeatLedPin, !digitalRead(HeatLedPin));
-      state = S_WAITTILLHOT;
-      break;
     case S_WAITTILLHOT:
-      digitalWrite(HeaterPin, LOW);
+      digitalWrite(HeaterPin,LOW);
       digitalWrite(PumpPin,HIGH);
       if(debug)
       {
         Serial.print("Temperature (WAITTILLHOT) = "); Serial.println(temperature);
       } 
-      digitalWrite(HeatLedPin, !digitalRead(HeatLedPin));
-      if(temperature>=30.0)
+      if((long)(time - waitBlink) >= 0)
+      {
+        waitBlink += Blinkdelay;
+        digitalWrite(HeatLedPin, !digitalRead(HeatLedPin));
+      }
+      if(temperature>=Temp_max)
       {
         digitalWrite(HeaterPin, HIGH);
         digitalWrite(HeatLedPin, LOW);
@@ -120,7 +118,7 @@ void loop() {
       {
         Serial.print("Temperature (CHECKINPUTS) = "); Serial.println(temperature);
       }
-      if(temperature<=27.0)
+      if(temperature<=Temp_min)
       {
         digitalWrite(ReadyLedPin, HIGH);
         state = S_WAITTILLHOT;
@@ -143,11 +141,13 @@ void loop() {
       }
       if (digitalRead(RunSwitchPin)==LOW)
       {
+        digitalWrite(HeaterPin,LOW);
         digitalWrite(PumpPin,LOW);
         state = S_RUN;
       }
       else
       {
+        digitalWrite(HeaterPin,HIGH);
         digitalWrite(PumpPin,HIGH);
         state = S_CHECKINPUTS;
       }     
@@ -157,14 +157,15 @@ void loop() {
       {
         Serial.print("Temperature (AUTOMATIC) = "); Serial.println(temperature);
       }
-      FogIntensity = (float) (analogRead(IntensityPin)*(100.0/1023.0));
+      FogIntensity = (long)analogRead(IntensityPin)*10;
       if(debug)
       {
         Serial.print("Temperature (AUTOMATIC) = "); Serial.println(temperature);
         Serial.print("Intensity (AUTOMATIC) = "); Serial.print(FogIntensity); Serial.print("%");
       }   
       digitalWrite(PumpPin,LOW);
-      waitAutomatic=waitUntil+(long)((float)AutomaticTimeMax*FogIntensity);
+      digitalWrite(HeaterPin,LOW);
+      waitAutomatic=waitUntil+FogIntensity;
       state=S_AUTOMATIC_RUN;
       break;   
     case S_AUTOMATIC_RUN:
@@ -173,14 +174,21 @@ void loop() {
         Serial.print("Temperature (AUTOMATIC_RUN) = "); Serial.println(temperature);
         Serial.print("Time to Wait (AUTOMATIC_RUN) = "); Serial.print((long)(time-waitAutomatic)); Serial.print("%");
       }   
-      if((long)(time-waitAutomatic) >= 0){
-        digitalWrite(PumpPin,HIGH);
-        state = S_CHECKINPUTS;
+      if(digitalRead(AutomaticSwitchPin)==LOW)
+      {
+        if((long)(time-waitAutomatic) >= 0){
+          digitalWrite(PumpPin,HIGH);
+          state = S_CHECKINPUTS;
+        }
+      }
+      else
+      {
+          digitalWrite(HeaterPin,HIGH);
+          digitalWrite(PumpPin,HIGH);
+          state = S_CHECKINPUTS;
       }
       break;
   }
   waitUntil += Loopdelay;  // wait another interval cycle
   }
-  //Serial.println();
- // delay(100);
 }
